eb397c6c82df1299153747bc6d61e301e4179389
“abdullah017” <dev.abdullahtas@gmail.com>
2025-12-23 21:03:14 +0300

    initial project

---
+++ example/tr_payment_hub_example.dart
@@ -0,0 +1,6 @@
+import 'package:tr_payment_hub/tr_payment_hub.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/tr_payment_hub.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/tr_payment_hub_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
